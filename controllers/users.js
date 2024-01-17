ea04ffd4ac85eedc18ce3be6cf1bc1e38a2928b8
Vladimir Strugov <Strugov2001@yandex.ru>
2024-01-17 13:55:43 +0300

    add checking for duplicates

--- controllers/users.js
+++ controllers/users.js
@@ -1,100 +1,106 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-import { StatusCodes } from 'http-status-codes';
-import User from '../models/User.js';
-import generateToken from '../utils/jwt.js';
-import ApiError from '../utils/ApiError.js';
-import { ERROR_CODE_DUPLICATE_MONGO, SALT_ROUNDS } from '../utils/constants.js';
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+import { StatusCodes } from 'http-status-codes'
+import User from '../models/User.js'
+import generateToken from '../utils/jwt.js'
+import ApiError from '../utils/ApiError.js'
+import { ERROR_CODE_DUPLICATE_MONGO, SALT_ROUNDS } from '../utils/constants.js'
 
 export const login = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
   try {
     const user = await User.findOne({ email })
       .select('+password')
       .orFail(() => {
-        throw new Error('NotAutanticate');
-      });
-    const matched = await bcrypt.compare(String(password), user.password);
+        throw new Error('NotAutanticate')
+      })
+    const matched = await bcrypt.compare(String(password), user.password)
     if (!matched) {
-      throw new Error('NotAutanticate');
+      throw new Error('NotAutanticate')
     }
-    const token = generateToken({ _id: user._id });
-    return res.send({ token });
+    const token = generateToken({ _id: user._id })
+    return res.send({ token })
   } catch (err) {
     if (err.message === 'NotAutanticate') {
-      return next(ApiError.Unauthorized('Неправильные почта или пароль'));
+      return next(ApiError.Unauthorized('Неправильные почта или пароль'))
     }
-    return next(ApiError());
+    return next(ApiError())
   }
-};
+}
 
 export const createUser = async (req, res, next) => {
   try {
+    if (User.findOne({ email: req.email })) {
+      return next(ApiError.Conflict('Пользователь уже существует'))
+    }
     const newUser = await bcrypt
       .hash(req.body.password, SALT_ROUNDS)
-      .then((hash) => User.create({ ...req.body, password: hash }));
-    const token = generateToken({ _id: newUser._id });
+      .then(hash => User.create({ ...req.body, password: hash }))
+    const token = generateToken({ _id: newUser._id })
     return res.status(StatusCodes.CREATED).send({
       email: newUser.email,
       name: newUser.name,
       _id: newUser._id,
-      token,
-    });
+      token
+    })
   } catch (err) {
     if (err instanceof mongoose.Error.ValidationError) {
-      return next(ApiError.BadRequest('Переданы неверные данные'));
+      return next(ApiError.BadRequest('Переданы неверные данные'))
     }
 
     if (err.code === ERROR_CODE_DUPLICATE_MONGO) {
-      return next(ApiError.Conflict('Пользователь уже существует'));
+      return next(ApiError.Conflict('Пользователь уже существует'))
     }
 
-    return next(new ApiError());
+    return next(new ApiError())
   }
-};
+}
 
 export const getCurrentUser = async (req, res, next) => {
   try {
-    const user = await User.findById(req.user._id).orFail();
+    const user = await User.findById(req.user._id).orFail()
     return res.status(StatusCodes.OK).send({
       email: user.email,
-      name: user.name,
-    });
+      name: user.name
+    })
   } catch (err) {
     if (err instanceof mongoose.Error.CastError) {
-      return next(ApiError.BadRequest('Переданы неверные данные'));
+      return next(ApiError.BadRequest('Переданы неверные данные'))
     }
     if (err instanceof mongoose.Error.DocumentNotFoundError) {
-      return next(ApiError.NotFound('Пользователь не найден'));
+      return next(ApiError.NotFound('Пользователь не найден'))
     }
 
-    return next(new ApiError());
+    return next(new ApiError())
   }
-};
+}
 
 export const updateInfoProfile = async (req, res, next) => {
   try {
-    const { email, name } = req.body;
+    const { email, name } = req.body
+    if (User.findOne({ email: email })) {
+      return next(ApiError.Conflict('Пользователь уже существует'))
+    }
     const updatedInfo = await User.findByIdAndUpdate(
       req.user._id,
       { email, name },
       {
         new: true,
-        runValidators: true,
-      },
-    ).orFail();
+        runValidators: true
+      }
+    ).orFail()
     return res.json({
       email: updatedInfo.email,
-      name: updatedInfo.name,
-    });
+      name: updatedInfo.name
+    })
   } catch (err) {
     if (err instanceof mongoose.Error.CastError) {
-      return next(ApiError.BadRequest('Переданы неверные данные'));
+      return next(ApiError.BadRequest('Переданы неверные данные'))
     }
     if (err instanceof mongoose.Error.DocumentNotFoundError) {
-      return next(ApiError.NotFound('Пользователь не найден'));
+      return next(ApiError.NotFound('Пользователь не найден'))
     }
 
-    return next(new ApiError());
+    return next(new ApiError())
   }
-};
+}
